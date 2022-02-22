21bdd4a2578840c0a7d67d0ecb06accbbd5a26a3
BennyGRIT <89871085+BennyGRIT@users.noreply.github.com>
2022-02-22 14:20:13 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,46 @@
+// //övning 1 - Klass
+// function Person (characterTrait, hariColor, name) {
+//     this.characterTrait = characterTrait;
+//     this.hariColor = hariColor;
+//     this.name = name;
+// }
+
+// Person.prototype.intro = function(){
+//     console.log(`This is ${this.name}`)
+// }
+
+// Person.prototype.datingIntro = function(){
+//     console.log(`I am ${this.name}, my hair is ${this.hariColor}, and I am extremly ${this.characterTrait}. Please date me!`)
+// }
+
+// const shayaan = new Person('unknowing', 'svart', 'Shayaan');
+// shayaan.datingIntro();
+
+// //Övning 2 - Klass
+// class Flowers{
+//     water = 'little';
+//     constructor(type, color, blooms){
+//         this.type = type;
+//         this.color = color;
+//         this.blooms = blooms;
+//     }
+
+//     presentation(){
+//         console.log(`${this.type} är en mycket fin blomma, och jag behöver sol`);
+//         if(this.blooms){
+//             console.log('Jag behöver VATTEN FFS')
+//         }
+//         else{
+//             console.log('Ge mig sol för fan')
+//         }
+//         console.log(this.water)
+//     }
+// }
+
+// const zenia = new Flowers('flower', 'lila', true);
+// const ivy = new Flowers('ivy', 'green', false);
+
+// zenia.presentation();
+// ivy.presentation();
+
+
